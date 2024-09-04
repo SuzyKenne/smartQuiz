5031e9b88650b346a225ff4b43925f3fea69bf4f
SuzyKenne <suzyndatewo@gmail.com>
2024-09-04 01:51:51 +0100

    timer component

---
+++ src/components/countDownTimer/CountDownTimer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function CountDownTimer() {
+    return (
+        <div className="CountDownTimer">
+            <h2>Time:</h2>
+            <div className="container-time">
+
+            </div>
+        </div>
+    )
+}
+
+
+export default CountDownTimer;
