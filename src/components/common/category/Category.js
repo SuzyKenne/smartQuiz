472113622677f42f40f2205bb41c8de7c4cae5f0
SuzyKenne <suzyndatewo@gmail.com>
2024-09-04 17:52:11 +0100

    category component updated

--- src/components/common/category/Category.js
+++ src/components/common/category/Category.js
@@ -1,27 +1,30 @@
-import React from "react";
-import { Link } from 'react-router-dom'
-import logo from '../../../assets/logo/logo2.jpg';
+import React, { useState } from "react";
+// import { Link } from 'react-router-dom'
+// import logo from '../../../assets/logo/logo2.jpg';
 import './Category.css';
-import Header from "../header/Header";
-import Footer from "../footer/Footer";
+// import Header from "../header/Header";
+// import Footer from "../footer/Footer";
+import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
-function Category() {
+function Category(props) {
+
+  const {label, options} = props;
+  const [value, setValue] = useState("");
+  const  handleChange = (e) => {
+    setValue(e.target.value);
+  }
   return (
     <div className="Category">
-      <Header />
-       <div className="category-section">        
-          <div className="grid-category">        
-            <Link className="category-item">
-              <img src={logo} alt='logo' width={50}/>
-              <div className="category-info">
-                <h3>General Knowledge</h3>
-                <p>Software Tester</p>
-              </div>
-              <i className="fa-solid fa-ellipsis-vertical options-icon"></i>
-            </Link>
-          </div>
-        </div>
-      <Footer />
+      <Box m={3} width="80%">
+        <FormControl size="small" fullWidth>
+          <InputLabel>{label}</InputLabel>
+          <Select value={value} label={label} onChange={handleChange}>
+           {options.map(({id, name}) => (
+            <MenuItem value={id} key={id}>{name}</MenuItem>
+           ))}
+          </Select>
+        </FormControl>
+      </Box>
     </div>
     
   )
