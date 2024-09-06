6989cc83089355983cf52519f8a3b1ef08a377ce
SuzyKenne <suzyndatewo@gmail.com>
2024-09-06 05:47:08 +0100

    updated quiz page

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -5,15 +5,25 @@ import Header from "../common/header/Header";
 import Footer from "../common/footer/Footer";
 import CountDownTimer from "../countDownTimer/CountDownTimer";
 import UseAxios from "../../hooks/UseAxios";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Box, CircularProgress, Typography } from "@mui/material";
+import { handleScoreChange } from "../../redux/action";
+import {decode} from 'html-entities';
 
 const getRandomInt = (max) =>{
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+const TIME_PER_QUESTION = {
+    easy: 5,
+    medium: 10,
+    hard: 15
+  };
 
 function Quiz() {
+
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [answers, setAnswers] = useState([]);
 
   const {
     questionCategory,
@@ -23,18 +33,20 @@ function Quiz() {
     score
   } = useSelector(state => state);
 
-  const history = useNavigate();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   let apiUrl = `/api.php?amount=${questionAmount}`;
   
-  if(!questionCategory){
+  if(questionCategory){
     apiUrl = apiUrl.concat(`&categories=${questionCategory}`)
   }
 
-  if(!questionDifficulty){
+  if(questionDifficulty){
     apiUrl = apiUrl.concat(`&difficulties=${questionDifficulty}`)
   }
 
-  if(!questionType){
+  if(questionType){
     apiUrl = apiUrl.concat(`&type=${questionType}`)
   }
    
@@ -44,6 +56,18 @@ function Quiz() {
 
   const [questionIndex, setQuestionIndex] = useState(0);
   const [option, setOption] = useState([]);
+
+  const [totalTime, setTotalTime] = useState(0);
+
+  useEffect(() => {
+    if(response?.results.length) {
+      setTotalTime(response.results.length * 5);
+    }
+  }, [response]);
+
+  const handleTimeUp = () => {
+    navigate('/result');
+  }
 
   
 
@@ -59,8 +83,19 @@ function Quiz() {
         question.correct_answer
       );
       setOption(answers);
+
+      const timePerQuestion = TIME_PER_QUESTION[questionDifficulty];
+      setTotalTime (response.results.length * timePerQuestion)
     }
-  }, [response, questionIndex ]);
+  }, [response, questionIndex , questionDifficulty]);
+
+
+  //initialize the answers array
+  useEffect(() => {
+    if(response?.results.length){
+      setAnswers(new Array(response.results.length).fill(null));
+    }
+  }, [response]);
  
 //  console.log(response)
 
@@ -73,13 +108,55 @@ function Quiz() {
     );
   
  }
+  //update selected answer
+  // const handleAnswerSelected = (answer) => {
+  //   setSelectedAnswer(answer);
+  //   const newAnswers = [...answers];
+  //   newAnswers[questionIndex] = answer;
+  //   setAnswers(newAnswers);
+  // }
 
-  const handleClickAnswer = () => {
+  //switch to the next question
+  const handleNext = () => {
     if(questionIndex + 1 < response.results.length){
       setQuestionIndex(questionIndex + 1);
-   } else {
+      setSelectedAnswer(answers[questionIndex + 1]);
+    }
+  }
 
-    history.push('/result');
+  //move to the previous question
+  const handleBack = () => {
+    if(questionIndex > 0){
+      setQuestionIndex(questionIndex - 1);
+      setSelectedAnswer(answers[questionIndex - 1]);
+    }
+  }
+
+  //handle the final score
+  const handleSubmit = () => {
+    let newScore = 0;
+    answers.forEach((answer, index) => {
+      if(answer === response.results[index].correct_answer){
+        newScore += 1;
+      }
+    });
+    dispatch(handleScoreChange(newScore));
+    // navigate('/result');
+
+  }
+
+  const handleClickAnswer = (e) => {
+    const question = response.results[questionIndex];
+    if(e.target.textContent === question.correct_answer){
+      dispatch(handleScoreChange(score + 1))
+    }
+
+    if(questionIndex + 1 < response.results.length){
+      setQuestionIndex(questionIndex + 1);
+   }
+   else {
+
+    navigate('/result');
    }
 }
   return (
@@ -87,21 +164,36 @@ function Quiz() {
         <Header />
         <div className="quiz-content">
             <h1>Good luck for your Quiz!</h1>
-            <CountDownTimer seconds={90}/>
-            <div className="quiz-question">              
-              <h2>Question {questionIndex + 1}: {response.results[questionIndex].question}</h2>
+            {totalTime > 0 && (
+              <CountDownTimer initialTime={totalTime} onTimeUp={handleTimeUp}/>
+            )}
+            
+            <div className="quiz-question">
+            <h2>Question {questionIndex + 1}: {decode(response.results[questionIndex].question)}</h2>
+            {/* {option.map( (data, id) => (
+                  <div key={id} className="answer-options">
+                    <button 
+                      onClick={() => handleAnswerSelected(data)
+                      } className={`answer-button ${selectedAnswer ===  data 
+                      ? 'selected' : '#4a90e2'}}`}>
+                      {decode(data)}
+                    </button>
+                 </div> 
+              ))}   */}
+
               {option.map( (data, id) => (
                   <div key={id} className="answer-options">
-                    <button onClick={handleClickAnswer} className="answer-button">{data}</button>
+                    <button onClick={handleClickAnswer} className={`answer-button ${selectedAnswer ===  data 
+                      ? 'selectedAnswer' : '#4a90e2'}`}>{decode(data)}</button>
                  </div> 
-              ))}                           
+              ))}                                        
             </div>
             <div className="quiz-navigation">
-              <Link className="nav-button">Back</Link>
+              <Link onClick={handleBack} disabled={questionIndex === 0} className="nav-button">Back</Link>
               <h2>{score} : {response.results.length}</h2>
-              <Link className="nav-button">Next</Link>
+              <Link onClick={handleNext} className="nav-button" >Next</Link>
             </div>
-            <Link to='/result' className="get-started-button">Submit</Link>
+            <Link onClick={handleSubmit} className="get-started-button" to='/result'>Submit</Link>
         </div>
         <Footer />
     </div>
